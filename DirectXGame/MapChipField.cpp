67822bf855305bf079c4fed7fe40b1ea38b9ba8a
OgawaRyo529 <k023g1073@g.neec.ac.jp>
2024-06-07 11:25:44 +0900

    確認課題スライド16まで

--- DirectXGame/MapChipField.cpp
+++ DirectXGame/MapChipField.cpp
@@ -1,1 +1,60 @@
-﻿
+﻿#include <map>
+#include<sstream>
+#include <cassert>
+#include "MapChipField.h"
+#include <string>
+#include<fstream>
+
+namespace {
+
+	std::map<std::string,MapChipType>mapChipTable={
+		{"0",MapChipType::kBlank},
+		{"1",MapChipType::kBlock},
+	};
+
+}
+
+void MapChipField::ResetMapChipData()
+{
+	//マップチップデータをリセット
+	mapChipData_.data.clear();
+	mapChipData_.data.resize(kNumBlockVirtical);
+	for (std::vector<MapChipType>& mapChipDataLine : mapChipData_.data) {
+		mapChipDataLine.resize(kNumBlockHorizontal);
+	}
+}
+
+void MapChipField:: LoadMapChipeCsv(const std::string& filePath) {
+	
+	ResetMapChipData();
+	
+	std::ifstream file;
+	file.open(filePath);
+	assert(file.is_open());
+
+	std::stringstream mapChipCsv;
+
+	mapChipCsv<<file.rdbuf();
+
+	file.close();
+
+	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+		std::string line;
+		getline(mapChipCsv,line);
+
+		std::istringstream line_stream(line);
+
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+			std::string word;
+			getline(line_stream,word,',');
+
+			if (mapChipTable.contains(word)) {
+				mapChipData_.data[i][j]=mapChipTable[word];
+			}
+		}
+
+	}
+
+
+
+}
--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -1,23 +1,34 @@
 ﻿#include <cstdint>
 #include <vector>
-
-class MapChipField{
-	//1ブロックのサイズ
-	static inline const float kBlockWidth=1.0f;
-	static inline const float kBlockHeight=1.0f;
-	//ブロックの個数
-	static inline  const uint32_t kNumBlockVirtical = 20;
-	static inline  const uint32_t kNumBlockHorizontal=100;
-};
+#include<stdint.h>
+#include"Vector3.h"
 
 enum class MapChipType{
 	kBlank,
 	kBlock,
 
 };
-
-struct MapChipData {
+struct  MapChipData {
 	std::vector<std::vector<MapChipType>>data;
-
-	MapChipData *mapChipData_;
 };
+
+class MapChipField{
+public:
+	//1ブロックのサイズ
+	static inline const float kBlockWidth=1.0f;
+	static inline const float kBlockHeight=1.0f;
+
+	//ブロックの個数
+	static inline  const uint32_t kNumBlockVirtical = 20;
+	static inline  const uint32_t kNumBlockHorizontal=100;
+
+private:
+	MapChipData mapChipData_;
+void ResetMapChipData();
+
+void LoadMapChipeCsv(const std::string& filePath);
+
+};
+
+
+
